f17bc77d494b3107dae6a71641d31ef9838ce5fe
KishalanPather <119253561+KishalanPather@users.noreply.github.com>
2026-08-13 20:26:41 +0200

    Initial Commit

---
+++ antidoomscroller2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  antidoomscroller2
+//
+//  Created by Kishalan Pather on 2026/08/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ antidoomscroller2/antidoomscroller2App.swift
@@ -0,0 +1,17 @@
+//
+//  antidoomscroller2App.swift
+//  antidoomscroller2
+//
+//  Created by Kishalan Pather on 2026/08/13.
+//
+
+import SwiftUI
+
+@main
+struct antidoomscroller2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
