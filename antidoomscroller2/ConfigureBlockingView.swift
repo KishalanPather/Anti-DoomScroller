990c0a3ffe54727f1b250a8bc6a0ead385e12824
KishalanPather <119253561+KishalanPather@users.noreply.github.com>
2026-08-22 11:44:43 +0200

    added form frontend

--- antidoomscroller2/ConfigureBlockingView.swift
+++ antidoomscroller2/ConfigureBlockingView.swift
@@ -15,21 +15,60 @@ struct ConfigureBlockingView: View{
     
     
     @State private var showingPicker = false
-    @State private var selection = FamilyActivitySelection()
     @State private var appState = AppState.inactive
     @State private var restrictionEndsAt = false
+    
+    @State private var selection = FamilyActivitySelection()
+    @State private var scrollLimit = 0
+    @State private var lockoutPeriod = 5
     
     
     var body: some View {
         VStack{
             Text("Configure blocking page")
             
+            if appState == .inactive{
+                Text("State: Inactive")
+            } else if appState == .monitoring {
+                Text("State: Monitoring")
+            }else if appState == .restricted {
+                Text("State: Restricted")
+            }
+            
+            Form{
+                Section(header:Text("Select offending apps")){
+                    Button("Choose Apps"){
+                        showingPicker = true
+                    }
+                    .familyActivityPicker(isPresented: $showingPicker, selection: $selection)
+                }
+                
+                Section(header: Text("Select scroll time (minutes)")){
+                    TextField("Scroll limit (minutes)", value:$scrollLimit, format: .number)
+                        .keyboardType(.numberPad)
+                }
+                
+                Section(header: Text("Select lockout period duration (minutes)")){
+                    TextField("LockoutPeriod (minutes)", value:$lockoutPeriod, format: .number)
+                        .keyboardType(.numberPad)
+                }
+                
+                Section{
+                    Button("Save"){submitForm()}.buttonStyle(.borderedProminent)
+                }
+                
+            }
+        
+            
         }
         
-    
+        
+    }
+    private func submitForm(){
+        print("Form submitted")
     }
     
     
 }
 
-//#Preview {ConfigureBlockingView()}
+#Preview {ConfigureBlockingView()}
