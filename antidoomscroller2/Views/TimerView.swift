e0167e1531df554b4c31c3a8604493e32824e015
KishalanPather <119253561+KishalanPather@users.noreply.github.com>
2026-09-06 10:01:48 +0200

    removed redundant timer code

--- antidoomscroller2/Views/TimerView.swift
+++ antidoomscroller2/Views/TimerView.swift
@@ -9,30 +9,6 @@ import SwiftUI
 
 struct TimerView: View{
     
-    func getDate() -> Date{
-        var components = DateComponents()
-        components.year = 2026
-        components.month = 9
-        components.day = 5
-        components.hour = 21  // Use 24-hour format (10 AM)
-        components.minute = 0
-        
-        let calendar = Calendar.current
-        guard let specificDate = calendar.date(from: components) else { return Date() }
-        
-        let newDate = Calendar.current.date(byAdding: .minute, value: 120, to: specificDate)!
-        return newDate
-        
-    }
-    
-    func calculateRestrictionEnd() -> Date {
-        let newDate = Calendar.current.date(byAdding: .minute, value: 120, to: AppGroupStateStore.shared.restrictionStartsAt)!
-        AppGroupStateStore.shared.restrictionEndsAt = newDate
-        return newDate
-    }
-    
-    
-    
     var body: some View {
         Text(AppGroupStateStore.shared.restrictionEndsAt, style: .timer)
             .font(.title)
